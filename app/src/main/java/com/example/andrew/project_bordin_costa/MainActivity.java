09b64ea2207a90e7bb8bcc3120c37d4bb2ed69c0
Andrew Bordin <bordinan@sheridancollege.ca>
2017-12-27 14:29:41 -0500

    Start of the project

---
+++ app/src/main/java/com/example/andrew/project_bordin_costa/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.andrew.project_bordin_costa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
