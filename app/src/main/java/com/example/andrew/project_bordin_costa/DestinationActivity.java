127d32a1762e3bfa725ddbde53f82b96f3dc80ac
Andrew Bordin <bordinan@sheridancollege.ca>
2017-12-28 16:45:48 -0500

    All activities are now connected and moving to one another.

--- app/src/main/java/com/example/andrew/project_bordin_costa/DestinationActivity.java
+++ app/src/main/java/com/example/andrew/project_bordin_costa/DestinationActivity.java
@@ -4,26 +4,45 @@ import android.app.Activity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import com.example.andrew.project_bordin_costa.model.Destination;
 
 import java.util.ArrayList;
 
-public class DestinationActivity extends Activity {
+public class DestinationActivity extends Activity implements View.OnClickListener{
 
     ArrayList<Destination> destinationArrayList;
     Button btnDatePicker;
-
-
+    Button btnThingsToDo;
+    Button btnRestaurants;
+    Button btnMap;
+    Button btnEmergency;
+    private static final int THINGS_TO_DO_ACTIVITY = 3;
+    private static final int RESTAURANT_ACTIVITY = 4;
+    private static final int MAP_ACTIVITY = 5;
+    private static final int EMERGENCY_ACTIVITY = 6;
+    int arrayPosition;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destination);
-
         btnDatePicker = findViewById(R.id.btnDateButton);
+
+        btnThingsToDo = findViewById(R.id.btnThingstodo);
+        btnThingsToDo.setOnClickListener(this);
+
+        btnRestaurants = findViewById(R.id.btnRestaurants);
+        btnRestaurants.setOnClickListener(this);
+
+        btnMap = findViewById(R.id.btnMap);
+        btnMap.setOnClickListener(this);
+
+        btnEmergency = findViewById(R.id.btnEmergency);
+        btnEmergency.setOnClickListener(this);
 
         Intent intent = getIntent();
 
@@ -31,16 +50,47 @@ public class DestinationActivity extends Activity {
         {
             destinationArrayList = (ArrayList<Destination>)intent.getSerializableExtra("data");
 
-            int arrayPosition = intent.getIntExtra("arrayPosition", 0);
+            arrayPosition = intent.getIntExtra("arrayPosition", 0);
 
             String cityname = destinationArrayList.get(arrayPosition).getCityName();
 
             btnDatePicker.setText(cityname + ": Pick a Travel Date");
 
             btnDatePicker.setBackgroundResource(destinationArrayList.get(arrayPosition).getCityPicture());
+        }
+    }
 
+    @Override
+    public void onClick(View view) {
+        Intent intent;
+        switch(view.getId()){
+            case R.id.btnThingstodo:
+                //code
+                intent = new Intent(getApplicationContext(),ThingsToDoActivity.class);
+                startActivityForResult(intent, THINGS_TO_DO_ACTIVITY);
+                intent.putExtra("arrayPosition", arrayPosition);
+                break;
 
+            case R.id.btnRestaurants:
+                //code
+                intent = new Intent(getApplicationContext(),RestaurantActivity.class);
+                startActivityForResult(intent, RESTAURANT_ACTIVITY);
+                intent.putExtra("arrayPosition", arrayPosition);
+                break;
 
+            case R.id.btnMap:
+                //code
+                intent = new Intent(getApplicationContext(),MapActivity.class);
+                startActivityForResult(intent, MAP_ACTIVITY);
+                intent.putExtra("arrayPosition", arrayPosition);
+                break;
+
+            case R.id.btnEmergency:
+                //code
+                intent = new Intent(getApplicationContext(),EmergencyActivity.class);
+                startActivityForResult(intent, EMERGENCY_ACTIVITY);
+                intent.putExtra("arrayPosition", arrayPosition);
+                break;
         }
     }
 }
