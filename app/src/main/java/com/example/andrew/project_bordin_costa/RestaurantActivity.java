2897aea019c94e55b7ba0e7b8957ae340a7168c4
Fernando Costa <costalu@sheridancollege.ca>
2017-12-30 15:15:58 -0500

    Updated design of RestaurantActivity so it looks better. Had to remove the string builder and separate each component into their own TextView so they could be styled separately.

--- app/src/main/java/com/example/andrew/project_bordin_costa/RestaurantActivity.java
+++ app/src/main/java/com/example/andrew/project_bordin_costa/RestaurantActivity.java
@@ -3,8 +3,11 @@ package com.example.andrew.project_bordin_costa;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,19 +25,40 @@ public class RestaurantActivity extends Activity {
     int arrayPosition;
 
     TextView txtRestaurant1;
-    TextView txtRestaurant2;
-    TextView txtInfoR1;
-    TextView txtInfoR2;
+    TextView txtFoodType1;
+    TextView txtAddress1;
+    TextView txtStars1;
+    TextView txtReview1;
+    TextView txtReviewBy1;
+    ImageView imgRestaurant1;
+    Button btnWebsite1;
 
     String restaurant1TXT;
-    String restaurant2TXT;
-    String infor1;
-    String infor2;
-
-    ImageView imgRestaurant1;
-    ImageView imgRestaurant2;
-
+    String foodType1;
+    String address1;
+    String stars1;
+    String review1;
+    String reviewBy1;
+    String website1;
     final int RESTAURANT_1 = 0;
+
+    TextView txtRestaurant2;
+    TextView txtFoodType2;
+    TextView txtAddress2;
+    TextView txtStars2;
+    TextView txtReview2;
+    TextView txtReviewBy2;
+    ImageView imgRestaurant2;
+    Button btnWebsite2;
+
+
+    String restaurant2TXT;
+    String foodType2;
+    String address2;
+    String stars2;
+    String review2;
+    String reviewBy2;
+    String website2;
     final int RESTAURANT_2 = 1;
 
     @Override
@@ -43,16 +67,39 @@ public class RestaurantActivity extends Activity {
         setContentView(R.layout.activity_restaurant);
 
         txtRestaurant1 = findViewById(R.id.txt_restaurant1);
-        txtRestaurant2 = findViewById(R.id.txt_restaurant2);
-        txtInfoR1 = findViewById(R.id.txt_info_r1);
-        txtInfoR2 = findViewById(R.id.txt_info_r2);
+        txtFoodType1 = findViewById(R.id.txt_foodType1);
+        txtAddress1 = findViewById(R.id.txt_address1);
+        txtStars1 = findViewById(R.id.txt_stars1);
+        txtReview1 = findViewById(R.id.txt_review1);
+        txtReviewBy1 = findViewById(R.id.txt_reviewBy1);
         imgRestaurant1 = findViewById(R.id.img_restaurant1);
+        btnWebsite1 = findViewById(R.id.btnWebsite1);
+
+        txtRestaurant2 = findViewById(R.id.txt_restaurant2);
+        txtFoodType2 = findViewById(R.id.txt_foodType2);
+        txtAddress2 = findViewById(R.id.txt_address2);
+        txtStars2 = findViewById(R.id.txt_stars2);
+        txtReview2 = findViewById(R.id.txt_review2);
+        txtReviewBy2 = findViewById(R.id.txt_reviewBy2);
         imgRestaurant2 = findViewById(R.id.img_restaurant2);
+        btnWebsite2 = findViewById(R.id.btnWebsite2);
 
         restaurant1TXT = "";
+        foodType1 = "";
+        address1 = "";
+        stars1 = "";
+        review1 = "";
+        reviewBy1 = "";
+        website1 = "";
+
+
         restaurant2TXT = "";
-        infor1 = "";
-        infor2 = "";
+        foodType2 = "";
+        address2 = "";
+        stars2 = "";
+        review2 = "";
+        reviewBy2 = "";
+        website2 = "";
 
 
         Intent intent = getIntent();
@@ -63,6 +110,20 @@ public class RestaurantActivity extends Activity {
             loadImages(arrayPosition);
             setInfo();
         }
+
+        btnWebsite1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openURL(website1);
+            }
+        });
+
+        btnWebsite2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openURL(website2);
+            }
+        });
     }
 
     public void loadInfo(int position) {
@@ -133,41 +194,20 @@ public class RestaurantActivity extends Activity {
             JSONObject restaurant2 = restaurants.getJSONObject(RESTAURANT_2);
 
             restaurant1TXT = restaurant1.getString("name");
+            foodType1 = "Food Type: " + restaurant1.getString("food-type");
+            address1 = restaurant1.getString("address");
+            stars1 = "Stars: " + restaurant1.getString("stars");
+            review1 = restaurant1.getString("review");
+            reviewBy1 = restaurant1.getString("review-by");
+            website1 = restaurant1.getString("website");
+
             restaurant2TXT = restaurant2.getString("name");
-
-            StringBuilder infoR1Builder = new StringBuilder();
-            StringBuilder infoR2Builder = new StringBuilder();
-
-
-
-            infoR1Builder.append("Food Type: ").append(restaurant1.getString("food-type"))
-                    .append("\n")
-                    .append("Address: ").append(restaurant1.getString("address"))
-                    .append("\n").append("\n")
-                    .append("Website: ").append(restaurant1.getString("website"))
-                    .append("\n").append("\n")
-                    .append("Review By: ").append(restaurant1.getString("review-by"))
-                    .append("\n")
-                    .append("\"").append(restaurant1.getString("review")).append("\"")
-                    .append("\n").append("\n")
-                    .append("Stars: ").append(restaurant1.getString("stars"))
-                    .append("\n").append("\n");
-
-            infoR2Builder.append("Food Type: ").append(restaurant2.getString("food-type"))
-                    .append("\n")
-                    .append("Address: ").append(restaurant2.getString("address"))
-                    .append("\n").append("\n")
-                    .append("Website: ").append(restaurant2.getString("website"))
-                    .append("\n").append("\n")
-                    .append("Review By: ").append(restaurant2.getString("review-by"))
-                    .append("\n")
-                    .append("\"").append(restaurant2.getString("review")).append("\"")
-                    .append("\n").append("\n")
-                    .append("Stars: ").append(restaurant2.getString("stars"))
-                    .append("\n").append("\n");
-
-            infor1 = infoR1Builder.toString();
-            infor2 = infoR2Builder.toString();
+            foodType2 = "Food Type: " + restaurant2.getString("food-type");
+            address2 = restaurant2.getString("address");
+            stars2 = "Stars: " + restaurant2.getString("stars");
+            review2 = restaurant2.getString("review");
+            reviewBy2 = restaurant2.getString("review-by");
+            website2 = restaurant2.getString("website");
 
         } catch (JSONException e) {
             e.printStackTrace();
@@ -176,8 +216,32 @@ public class RestaurantActivity extends Activity {
 
     private void setInfo() {
         txtRestaurant1.setText(restaurant1TXT);
+        txtFoodType1.setText(foodType1);
+        txtAddress1.setText(address1);
+        txtStars1.setText(stars1);
+        txtReview1.setText(review1);
+        txtReviewBy1.setText(reviewBy1);
+
         txtRestaurant2.setText(restaurant2TXT);
-        txtInfoR1.setText(infor1);
-        txtInfoR2.setText(infor2);
+        txtFoodType2.setText(foodType2);
+        txtAddress2.setText(address2);
+        txtStars2.setText(stars2);
+        txtReview2.setText(review2);
+        txtReviewBy2.setText(reviewBy2);
+    }
+
+    private void openURL(String website1) {
+
+        //Create a new intent
+        Intent intent = new Intent();
+
+        // set the action to VIEW
+        intent.setAction(Intent.ACTION_VIEW);
+
+        // set the data of the Intent to be the Uri represented by the String
+        intent.setData(Uri.parse(website1));
+
+        // start activity that will handle Uri
+        startActivity(intent);
     }
 }
