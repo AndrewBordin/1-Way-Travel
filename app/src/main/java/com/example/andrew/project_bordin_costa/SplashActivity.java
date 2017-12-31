3da3c544684fe070b1148b33739aa3eeb7c68106
Andrew Bordin <bordinan@sheridancollege.ca>
2017-12-31 12:28:45 -0500

    Added a splashscreen for the app

---
+++ app/src/main/java/com/example/andrew/project_bordin_costa/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.andrew.project_bordin_costa;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
