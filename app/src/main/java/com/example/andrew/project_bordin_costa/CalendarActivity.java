6525c572822288078298bc4aabc83f4bcff529d8
Andrew Bordin <bordinan@sheridancollege.ca>
2017-12-31 00:31:45 -0500

    Calendar changes

--- app/src/main/java/com/example/andrew/project_bordin_costa/CalendarActivity.java
+++ app/src/main/java/com/example/andrew/project_bordin_costa/CalendarActivity.java
@@ -58,10 +58,10 @@ public class CalendarActivity extends Activity {
                         datePicker.getMonth() - 1,
                         datePicker.getDayOfMonth(),
                         0,
-                        00,
+                        30,
                         00);
 
-                notification.set(Calendar.AM_PM, Calendar.PM);
+                notification.set(Calendar.AM_PM, Calendar.AM);
 
                 if (userPick.compareTo(current) <= 0){
                     //the date chosen is in the past
