f738c79e878dc373fec843c31d0736ffc34a7c8e
Andrew Bordin <bordinan@sheridancollege.ca>
2018-01-01 13:51:32 -0500

    Commented the main activity aside from one method. String literals on the mainactivity have been changed to string resources.

--- app/src/main/java/com/example/andrew/project_bordin_costa/MainActivity.java
+++ app/src/main/java/com/example/andrew/project_bordin_costa/MainActivity.java
@@ -4,9 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.andrew.project_bordin_costa.model.Destination;
 
@@ -14,20 +13,26 @@ import java.util.ArrayList;
 
 public class MainActivity extends Activity implements View.OnClickListener{
 
+    //Variable Declarations
     Button btnToronto;
     Button btnLondon;
     Button btnSanFran;
     Button btnSydney;
     ArrayList<Destination> destinations;
-
-    private static final int DESTINATION_ACTIVITY = 2;
     int currentDestination = 0;
+    private static final int DESTINATION_ACTIVITY = 2;
 
+    //Run when the activity is created
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Setting the button variables to their corresponding buttons
+        //Setting each button to an onclicklistener
+
+        //NOTE* Instead of assigning each button to their individual onclicklistener, we decided
+        //to override the onclick method, and use a switch statement to account for all the button clicks
         btnToronto = findViewById(R.id.btnToronto);
         btnToronto.setOnClickListener(this);
 
@@ -40,8 +45,12 @@ public class MainActivity extends Activity implements View.OnClickListener{
         btnSydney = findViewById(R.id.btnSydney);
         btnSydney.setOnClickListener(this);
 
+        //Calling the getdestination method from the Destination POJO which creates an array with
+        //all of the destinations and information associated with them
         destinations = Destination.getDestination();
     }
+
+    //TODO:COMMENT THIS
     @Override
     public void onRestart() {
         super.onRestart();
@@ -62,41 +71,47 @@ public class MainActivity extends Activity implements View.OnClickListener{
         destinations = Destination.getDestination();
     }
 
+    //Overriden onclick method
     @Override
     public void onClick(View view) {
 
+        //Creating an intent which gets the application context and destination activity
         Intent intent = new Intent(getApplicationContext(),DestinationActivity.class);
+        //Giving the intent the destinations array
         intent.putExtra("data", destinations);
 
+        //switch that gets the id of the button click, runs the corresponding code and activity.
         switch(view.getId()){
 
             case R.id.btnToronto:
-                //code
+                //Setting the current destination
+                //giving the intent the current destination. The destination that was chosen.
                 currentDestination = 0;
                 intent.putExtra("arrayPosition", currentDestination);
                 startActivityForResult(intent, DESTINATION_ACTIVITY);
                 break;
 
             case R.id.btnLondon:
-                //code
                 currentDestination = 1;
                 intent.putExtra("arrayPosition", currentDestination);
                 startActivityForResult(intent, DESTINATION_ACTIVITY);
                 break;
 
             case R.id.btnSanFran:
-                //code
                 currentDestination = 2;
                 intent.putExtra("arrayPosition", currentDestination);
                 startActivityForResult(intent, DESTINATION_ACTIVITY);
                 break;
 
             case R.id.btnSydney:
-                //code
                 currentDestination = 3;
                 intent.putExtra("arrayPosition", currentDestination);
                 startActivityForResult(intent, DESTINATION_ACTIVITY);
                 break;
+
+            default:
+                //If no choice is made but the onclick still runs, launch a toast to the user.
+                Toast.makeText(getApplicationContext(), "Error: No destination chosen.", Toast.LENGTH_LONG);
         }
 
     }
