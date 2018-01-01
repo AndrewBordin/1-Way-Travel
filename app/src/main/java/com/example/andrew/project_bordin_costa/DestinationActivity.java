013ad9bc376b976a7514833c1df827bd45498173
Andrew Bordin <bordinan@sheridancollege.ca>
2018-01-01 14:09:37 -0500

    Commented the destination activity, and set all string literals to String resources.

--- app/src/main/java/com/example/andrew/project_bordin_costa/DestinationActivity.java
+++ app/src/main/java/com/example/andrew/project_bordin_costa/DestinationActivity.java
@@ -16,8 +16,8 @@ import java.util.ArrayList;
 
 public class DestinationActivity extends Activity implements View.OnClickListener{
 
+    //Variable Declarations
     ArrayList<Destination> destinationArrayList;
-    Button btnDatePicker;
     Button btnThingsToDo;
     Button btnRestaurants;
     Button btnMap;
@@ -33,20 +33,14 @@ public class DestinationActivity extends Activity implements View.OnClickListene
     private static final int CALENDAR_ACTIVITY = 7;
     int arrayPosition;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_destination);
-
+        //Setting the button variables to their corresponding buttons
+        //Setting each button to an onclicklistener
         fabDate = findViewById(R.id.fabDate);
         fabDate.setOnClickListener(this);
-
-        txtDestinationName = findViewById(R.id.txtDestName);
-
-        txtDestinationSubheading = findViewById(R.id.txtSubheading);
-
-        imageCity = findViewById(R.id.imageCity);
 
         btnThingsToDo = findViewById(R.id.btnThingstodo);
         btnThingsToDo.setOnClickListener(this);
@@ -60,34 +54,57 @@ public class DestinationActivity extends Activity implements View.OnClickListene
         btnEmergency = findViewById(R.id.btnEmergency);
         btnEmergency.setOnClickListener(this);
 
+        //Setting the textview variables to their corresponding textview
+        txtDestinationName = findViewById(R.id.txtDestName);
+        txtDestinationSubheading = findViewById(R.id.txtSubheading);
+
+        //Setting the imageview variable for the image of the city clicked to it's corresponding-
+        //imageview
+        imageCity = findViewById(R.id.imageCity);
+
+        //Getting the intent from the previous activity(MainActivity)
         Intent intent = getIntent();
 
+        //do if the intent has contents
         if(intent != null)
         {
+            //Getting the arraylist from the intent
             destinationArrayList = (ArrayList<Destination>)intent.getSerializableExtra("data");
 
+            //getting the arrayposition(Cityclicked) from the intent
             arrayPosition = intent.getIntExtra("arrayPosition", 0);
 
+            //Getting the name of the city from the destination pojo and setting it to a String
             String cityname = destinationArrayList.get(arrayPosition).getCityName();
 
+            //Getting the city subheading from the destination pojo and setting it to a String.
             String citySubheading = destinationArrayList.get(arrayPosition).getCitySubheading();
 
+            //Setting both the city name and subheading to Textviews.
             txtDestinationName.setText(cityname);
 
             txtDestinationSubheading.setText(citySubheading);
 
+            //Getting the drawable image from the destination POJO associated with the city and setting
+            //it to the imageCity imageview.
             imageCity.setImageResource(destinationArrayList.get(arrayPosition).getCityPicture());
         }
     }
 
+    //Overriden onclick method handling each buttonclick on the activity
     @Override
     public void onClick(View view) {
+
+        //Creating an intent which gets the application context and the chosen activity
         Intent intent;
+
+        //switch that gets the id of the button click, runs the corresponding code and activity.
         switch(view.getId()){
             case R.id.btnThingstodo:
-                //code
                 intent = new Intent(getApplicationContext(),ThingsToDoActivity.class);
+                //giving the intent the current destination. The destination that was chosen.
                 intent.putExtra("arrayPosition", arrayPosition);
+                //Launching the activity
                 startActivityForResult(intent, THINGS_TO_DO_ACTIVITY);
                 break;
 
